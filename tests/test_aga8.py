f64e990aeb1d22bfcdb04d54bedb26a5abc499b6
Roy Vegard Ovesen <rovo@equinor.com>
2025-01-09 10:44:46 +0100

    Use pytest's methods and add tests

--- tests/test_aga8.py
+++ tests/test_aga8.py
@@ -1,34 +1,50 @@
 import pyaga8
+from pytest import approx, raises
 
+
+gerg = pyaga8.Gerg2008()
+
+FULL_COMPOSITION = pyaga8.Composition()
+FULL_COMPOSITION.methane = 0.778_24
+FULL_COMPOSITION.nitrogen = 0.02
+FULL_COMPOSITION.carbon_dioxide = 0.06
+FULL_COMPOSITION.ethane = 0.08
+FULL_COMPOSITION.propane = 0.03
+FULL_COMPOSITION.isobutane = 0.001_5
+FULL_COMPOSITION.n_butane = 0.003
+FULL_COMPOSITION.isopentane = 0.000_5
+FULL_COMPOSITION.n_pentane = 0.001_65
+FULL_COMPOSITION.hexane = 0.002_15
+FULL_COMPOSITION.heptane = 0.000_88
+FULL_COMPOSITION.octane = 0.000_24
+FULL_COMPOSITION.nonane = 0.000_15
+FULL_COMPOSITION.decane = 0.000_09
+FULL_COMPOSITION.hydrogen = 0.004
+FULL_COMPOSITION.oxygen = 0.005
+FULL_COMPOSITION.carbon_monoxide = 0.002
+FULL_COMPOSITION.water = 0.000_1
+FULL_COMPOSITION.hydrogen_sulfide = 0.002_5
+FULL_COMPOSITION.helium = 0.007
+FULL_COMPOSITION.argon = 0.001
 
 def test_gerg2008_calc_density():
-    composition = pyaga8.Composition()
-    composition.methane = 0.778_24
-    composition.nitrogen = 0.02
-    composition.carbon_dioxide = 0.06
-    composition.ethane = 0.08
-    composition.propane = 0.03
-    composition.isobutane = 0.001_5
-    composition.n_butane = 0.003
-    composition.isopentane = 0.000_5
-    composition.n_pentane = 0.001_65
-    composition.hexane = 0.002_15
-    composition.heptane = 0.000_88
-    composition.octane = 0.000_24
-    composition.nonane = 0.000_15
-    composition.decane = 0.000_09
-    composition.hydrogen = 0.004
-    composition.oxygen = 0.005
-    composition.carbon_monoxide = 0.002
-    composition.water = 0.000_1
-    composition.hydrogen_sulfide = 0.002_5
-    composition.helium = 0.007
-    composition.argon = 0.001
-
-    gerg = pyaga8.Gerg2008()
-    gerg.set_composition(composition)
+    gerg.set_composition(FULL_COMPOSITION)
     gerg.temperature = 400.0
     gerg.pressure = 50_000.0
 
     gerg.calc_density(0)
-    assert abs(gerg.d - 12.798_286_260_820_62) < 1.0e-10
+    assert gerg.d == approx(12.798_286_260_820_62)
+
+def test_gerg2008_set_pressure():
+    gerg.pressure = 42.14
+    assert gerg.pressure == approx(42.14)
+
+    with raises(TypeError):
+        gerg.pressure = 'spam'
+
+def test_gerg2008_set_temperature():
+    gerg.temperature = 42.14
+    assert gerg.temperature == approx(42.14)
+
+    with raises(TypeError):
+        gerg.temperature = 'spam'
